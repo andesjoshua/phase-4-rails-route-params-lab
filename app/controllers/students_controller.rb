ee36c24f1735ec4dee4082d6b65cd0074abf1162
Joshua Andes <andesjoshua@comcast.net>
2021-11-22 12:24:40 -0500

    commit tree 69cd9abb81dd58459f00b43a932f98d9bfbfdd19

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    studentsById = Student.find(params[:id])
+    render json: studentsById
+  end
+
 end
